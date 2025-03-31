f62905bbf30893529bff17dd160b494dd18420b4
CodePandaaAI <romitsharmakv@gmail.com>
2025-03-31 23:40:17 +0530

    DSA
    New : ReverseString

---
+++ src/ReverseString.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val stringRev = "hello".toCharArray()
+    var tempC: Char
+    var left = 0
+    var right = stringRev.size -1
+    while (left<right){
+        tempC = stringRev[left]
+        stringRev[left] = stringRev[right]
+        stringRev[right] = tempC
+
+        left++
+        right--
+    }
+    println(String(stringRev))
+}
+
